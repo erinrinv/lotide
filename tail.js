81bb1697bb5f562c908e2f5eeef54a2d110a1712
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-05 13:47:52 -0700

    created tail.js

---
+++ tail.js
@@ -0,0 +1,25 @@
+// Paste assert from assertequal.js.
+// Tail will check array to see if its the same
+// but cant modify it so tail function will create new array without the
+// "head" element
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray[i - 1] = array[i];
+  }
+  return newArray;
+};
+
+// Test Case: Check the original array
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words);
+//assertEqual(words.length, 3);
