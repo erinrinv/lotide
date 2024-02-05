2f1e90a530e7f49588bbb2e0c6214c90e3140bae
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-05 13:25:23 -0700

    created head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+// Paste assertEqual function and created head function to pull first element of array and return it
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (array) {
+  return array[0];
+};
+
+//test code
+//assertEqual(head([5, 6, 7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5]), 5);
+//assertEqual(head([]), undefined);
