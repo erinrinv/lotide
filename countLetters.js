bc809a11011f7c7a7dc21cbccb92d5e5d2c800ab
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-06 18:42:33 -0700

    updated countletters with notes and fixed spacing

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 // Checks to see if equal
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -10,25 +10,29 @@ const assertEqual = function(actual, expected) {
 };
 
 // countLetters Function
-// The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence. 
+// The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
 
-const countLetters = function(string){
+const countLetters = function (string) {
   const count = {};
 
-  for(let letter of string){
-    if(letter !== " "){
-      if(count[letter]){
+  for (let letter of string) {
+    // filters if there are spaces in string
+    if (letter !== " ") {
+      // If letter has been already counted then it adds 1 to the count{}
+      if (count[letter]) {
         count[letter] += 1;
-
       }
-      else{
+      // If letter does not exist in the count after going through the loop then it creates it
+      else {
         count[letter] = 1;
       }
     }
-
   }
+  // returns the final result
   return count;
 };
 
 
-console.log(countLetters("lighthouse in the house"));
+// TEST CODE
+//Console.log(countLetters("lighthouse in the house");
+//assertEqual(countLetters("lighthouse in the house"));
