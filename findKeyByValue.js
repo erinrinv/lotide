3f3458f3c70c3f54a92bcabb3e62bbd90e0f1ee4
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-07 10:18:54 -0700

    updated FindKeyByValue.js based on compass ai suggestion feedback

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,9 +13,9 @@ const assertEqual = function (actual, expected) {
 //which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
 const findKeyByValue = function (object, value) {
   const keyArray = Object.keys(object);
-  for (let element of keyArray) {
-    if (object[element] === value) {
-      return element;
+  for (let key of keyArray) {
+    if (object[key] === value) {
+      return key;
     }
   }
 };
