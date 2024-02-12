bd981152e56d0aa99886612472c717fff3fb38ed
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-12 09:12:04 -0700

    Refactored eqArrays and created eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 // function to compare two arrays together and return true or false if they are equal
 
@@ -29,16 +20,4 @@ const eqArrays = function (array1, array2) {
   };
   
 
-// test code
-
- // eqArrays([1, 2, 3], [1, 2, 3]); // => true
- // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-  
- // eqArrays([1, 2, 3], [3, 2, 1]); // => false
- // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-  
-//  eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-//  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  
- // eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
- // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  module.exports = eqArrays;
