d5f7b531f6f4d5bd72448eb9e41929ee09bc8337
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-12 10:02:16 -0700

    set up moch and chai for tail and middle and test js for them as well

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,22 @@
 const assertEqual = require('../assertEqual');
 const tail = require("../tail");
-
+const assert = require('chai').assert;
 
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words);
+//assertEqual(words.length, 3);
+
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+});
